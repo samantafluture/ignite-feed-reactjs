1c38770547c5f8a1931594e50fae8ef1a54ea3ae
Samanta Fluture <samantafluture@gmail.com>
2022-09-07 17:06:34 -0400

    extract avatar component and create two visual variaties

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # ignite-feed-reactjs
+
+Simple React.js application that mimics a social media site similar do Reddit, with user profiles and shared posts focused on textual content. 
+
+## when to create a separated component in React?
+
+1. When something is being repeated too often
+   - With same visuals, behaviors and functionalities
+2. When you can remove something from a big component without breaking it
+   - Than, this big component can have a clearer focus
+   - Even though this new component is not reusable
+
+## props
+
+- When I want to use different visual characteristics and behaviors on a component, I should pass props 
+- It's a form of communication
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilSimple } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -10,11 +11,9 @@ export function Sidebar() {
 				src='https://images.unsplash.com/photo-1555949963-ff9fe0c870eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
 			/>
 
-			<div className={styles.profile}>
-				<img
-					className={styles.avatar}
-					src='https://github.com/samantafluture.png'
-				/>
+			<div className={styles.profile}>			
+				<Avatar src='https://github.com/samantafluture.png' />
+
 				<strong>Samanta Fluture</strong>
 				<span>Web Developer</span>
 			</div>
